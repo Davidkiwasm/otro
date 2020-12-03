7a6027663f14992624960393e2ec3a92512c7fe7
Davidkiwasm <75396520+Davidkiwasm@users.noreply.github.com>
2020-12-03 15:45:31 -0600

    programas
    programas

---
+++ arraylist/src/arraylist/n/papeleta.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraylist.n;
+
+/**
+ *
+ * @author david
+ */
+public class papeleta {
+    public String color;
+    public String candidato;
+    public papeleta(String color, String candidato){
+        this.color = color;
+        this.candidato = candidato;
+        
+    }
+    public String getColor() {
+        return color;
+        
+        
+    }
+    public String getCandidato(){
+        return candidato;
+}   
+}
